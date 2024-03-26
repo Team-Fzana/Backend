12a9213b51b3e05aa36bc6d39515c540aa56514c
jinyongkim123 <kjy154969@gmail.com>
2024-03-26 23:26:52 +0900

    clear git cache2

---
+++ fzana/src/main/java/com/example/fzana/FzanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.fzana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FzanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FzanaApplication.class, args);
+	}
+
+}
