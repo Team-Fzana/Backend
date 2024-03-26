6c0d1c232e719bf502d3a0ee8116939e158f800c
jinyongkim123 <kjy154969@gmail.com>
2024-03-26 23:26:52 +0900

    clear git cache2

---
+++ fzana/src/test/java/com/example/fzana/FzanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fzana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FzanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
