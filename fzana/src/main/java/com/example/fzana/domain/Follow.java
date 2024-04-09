e8ed4c5d95a1f9fca608e4ff8cff2feaa658900d
kingjinyong <wlsdydwhswkf@gmail.com>
2024-04-09 18:45:41 +0900

    Follow 테이블 생성

---
+++ fzana/src/main/java/com/example/fzana/domain/Follow.java
@@ -0,0 +1,30 @@
+package com.example.fzana.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Follow {
+    @Id
+    @GeneratedValue
+    @Column(name = "follow_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "following_id")
+    private User following;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower_id")
+    private User follower;
+
+    @Column(name = "created_at") // 데이터베이스 컬럼명 변경
+    @Temporal(TemporalType.TIMESTAMP) // TemporalType 설정 추가
+    private Date createdAt;
+
+    @Column(name = "updated_at") // 데이터베이스 컬럼명 변경
+    @Temporal(TemporalType.TIMESTAMP) // TemporalType 설정 추가
+    private Date updatedAt;
+
+}
