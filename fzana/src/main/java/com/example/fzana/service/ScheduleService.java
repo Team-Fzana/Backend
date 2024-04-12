d87e4404042a104ccc732dc3be4c723da4d73dc5
kingjinyong <wlsdydwhswkf@gmail.com>
2024-04-12 15:19:27 +0900

    일정 조회 기능 구현 완료

--- fzana/src/main/java/com/example/fzana/service/ScheduleService.java
+++ fzana/src/main/java/com/example/fzana/service/ScheduleService.java
@@ -7,6 +7,10 @@ import com.example.fzana.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ScheduleService {
@@ -14,14 +18,33 @@ public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
     private final UserRepository userRepository;        // 수연이가 해줄거임
 
+
+    /*
+     * todo-list, 일정 리스트 조회
+     */
+    public List<ScheduleForm> scheduleList(Long userId) {
+        // 1. 일정 & todo-list 조회
+        List<Schedule> schedules = scheduleRepository.findByUserId(userId);
+
+        // 2. 엔티티 -> DTO 변환
+        List<ScheduleForm> dtos = new ArrayList<ScheduleForm>();
+        for (int i = 0; i < schedules.size(); i++){ // 1. 조회한 일정 엔티티 수 만큼 반복하기
+            Schedule s = schedules.get(i);          // 2. 조회한 댓글 엔티티 하나씩 가져오기
+            ScheduleForm dto = ScheduleForm.createSchedule(s); // 3. 엔티티를 DTO로 변환
+            dtos.add(dto);                          // 4. 변환한 DTO를 dtos 리스트에 삽입
+        }
+
+        return dtos;
+    }
+
     /*
      * 일정 추가
      */
     public ScheduleForm createSchedule(Long userId, ScheduleForm scheduleForm) {
         // 1. 사용자 id 조회 및 예외 처리
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("댓글 생성 실패! " +
-                "대상 게시글이 없습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("일정 & todo-list 추가 실패! " +
+                "해당 사용자가 없습니다."));
 
         // 2. 일정 엔티티 생성
         Schedule schedule = Schedule.createSchedule(user, scheduleForm);
