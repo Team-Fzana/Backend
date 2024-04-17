52aea539e900b9721d26f62c5a2f39ca9e6fcc27
kingjinyong <wlsdydwhswkf@gmail.com>
2024-04-18 01:00:10 +0900

    Requestbody, Responses 수정, .env 깃 이그노어 추가

--- fzana/src/main/java/com/example/fzana/domain/Schedule.java
+++ fzana/src/main/java/com/example/fzana/domain/Schedule.java
@@ -1,25 +1,22 @@
 package com.example.fzana.domain;
 
-import com.example.fzana.dto.ScheduleForm;
+import com.example.fzana.dto.ScheduleRequest;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Schedule {
 
     @Id
     @GeneratedValue
-    @Column(name = "todolist_id")
+    @Column(name = "schedule_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY) // User 테이블 ID 와 다대일 관계
@@ -53,51 +50,42 @@ public class Schedule {
     @Temporal(TemporalType.TIMESTAMP) // TemporalType 설정 추가
     private Date updatedAt;
 
-    public Schedule(User user, String content, LocalDateTime thisDay, int checkStatus, Date startTime, Date endTime, LocalDateTime now, LocalDateTime now1) {
+
+    public Schedule(User user, String content, int checkStatus, LocalDateTime thisDay, Date startTime, Date endTime) {
         this.user = user;
         this.content = content;
         this.thisDay = thisDay;
         this.checkStatus = checkStatus;
         this.startTime = startTime;
         this.endTime = endTime;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
     }
 
-
-    public static Schedule createSchedule(User user, ScheduleForm scheduleForm) {
-        if (scheduleForm.getId() != null)
-            throw new IllegalArgumentException("일정 생성 실패! 일정의 id가 없어야 합니다!");
-        if (scheduleForm.getUserId() != user.getId())
-            throw new IllegalArgumentException("일정 생성 실패! 사용자의 id가 잘못 되었습니다!");
-
+    // DTO 데이터로 엔티티 생성
+    public static Schedule createSchedule(User user, ScheduleRequest scheduleRequest) {
         return new Schedule(
                 user,
-                scheduleForm.getContent(),
-                scheduleForm.getThisDay(),
-                scheduleForm.getCheckStatus(),
-                scheduleForm.getStartTime(),
-                scheduleForm.getEndTime(),
-                LocalDateTime.now(),
-                LocalDateTime.now()
+                scheduleRequest.getContent(),
+                scheduleRequest.getCheckStatus(),
+                scheduleRequest.getThisDay(),
+                scheduleRequest.getStartTime(),
+                scheduleRequest.getEndTime()
                 );
     }
 
 
-
-    public void patch(ScheduleForm scheduleForm) {
+    public void patch(ScheduleRequest scheduleRequest) {
         //예외 발생
-        if(this.id != scheduleForm.getId())
-            throw new IllegalArgumentException("일정 수정 실패!! 잘못된 일정 id가 입력 되었습니다!");
+//        if(this.id != scheduleRequest.getId())
+//            throw new IllegalArgumentException("일정 수정 실패!! 잘못된 일정 id가 입력 되었습니다!");
 
         // 객체 갱신
         // 변경할 일정 내용이 있다면
-        this.content = scheduleForm.getContent(); // 내용 변경
+        this.content = scheduleRequest.getContent(); // 내용 변경
 
-        this.checkStatus = scheduleForm.getCheckStatus(); // 상태 변경
+        this.checkStatus = scheduleRequest.getCheckStatus(); // 상태 변경
 
-        this.thisDay = scheduleForm.getThisDay(); // 등록 날짜 변경
-        this.startTime = scheduleForm.getStartTime(); // 시작 시간 변경
-        this.endTime = scheduleForm.getEndTime(); //종료 시간 변경
+        this.thisDay = scheduleRequest.getThisDay(); // 등록 날짜 변경
+        this.startTime = scheduleRequest.getStartTime(); // 시작 시간 변경
+        this.endTime = scheduleRequest.getEndTime(); //종료 시간 변경
     }
 }
