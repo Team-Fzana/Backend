5e7f3d3da4a7300445f547e8dd37c2b64595f368
kingjinyong <wlsdydwhswkf@gmail.com>
2024-04-23 00:01:23 +0900

    스케줄, 멤버 컨트롤러에도 스웨거 summary, description 작성 해주었음

--- fzana/src/main/java/com/example/fzana/controller/MemberController.java
+++ fzana/src/main/java/com/example/fzana/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.example.fzana.controller;
 import com.example.fzana.domain.Member;
 import com.example.fzana.dto.*;
 import com.example.fzana.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -24,6 +25,7 @@ public class MemberController {
 
     // 회원가입
     @PostMapping("/signup")
+    @Operation(summary = "회원가입", description = "이메일, 비밀번호, 닉네임, 소개글을 등록하여 회원가입을 합니다.")
     public ResponseEntity<Member> signUp(@RequestBody MemberForm memberForm) {
         Member member = memberService.signUp(
                 memberForm.getEmail(),
@@ -36,6 +38,7 @@ public class MemberController {
 
     // 이메일 중복 확인
     @GetMapping("/valid")
+    @Operation(summary = "유효성 검사", description = "이메일 형식, 중복 확인을 합니다.")
     public ResponseEntity<String> checkDuplicateEmail(@RequestParam String email) {
         memberService.validateDuplicateEmail(email);
         return ResponseEntity.ok("사용 가능한 이메일입니다.");
@@ -44,12 +47,14 @@ public class MemberController {
 
     // 로그인
     @PostMapping("/login")
+    @Operation(summary = "로그인", description = "이메일, 비밀번호로 로그인 합니다.")
     public ResponseEntity<Member> signIn(@RequestParam String email, @RequestParam String password) {
         Member member = memberService.signIn(email,password);
         return ResponseEntity.ok(member);
     }
 
     @PostMapping("/logout")
+    @Operation(summary = "로그아웃", description = "로그아웃을 합니다.")
     public ResponseEntity<String> signOut() {
         memberService.signOut();
         return ResponseEntity.ok("로그아웃 성공");
@@ -57,13 +62,15 @@ public class MemberController {
 
     // 사용자 정보 불러오기
     @GetMapping("/members/{memberId}")
+    @Operation(summary = "사용자 정보 불러오기", description = "사용자의 불러옵니다.")
     public ResponseEntity<MemberInfoResponse> memberInfo(@PathVariable Long memberId){
         MemberInfoResponse infos = memberService.bringInfo(memberId);
         return ResponseEntity.status(HttpStatus.OK).body(infos);
     }
 
-    // 사용자 닉네임 입력 & 수정
+    // 사용자 닉네임 수정
     @PostMapping("/members/{memberId}/nickname")
+    @Operation(summary = "사용자 닉네임 수정", description = "사용자의 닉네임을 수정합니다.")
     public ResponseEntity<NicknameResponse> submitNickname(@PathVariable Long memberId,
                                                            @RequestBody NicknameRequest nicknameRequest){
         NicknameResponse updated = memberService.submitNickname(memberId, nicknameRequest);
@@ -73,6 +80,7 @@ public class MemberController {
 
     // 사용자 소개글 입력 & 수정
     @PostMapping("/members/{memberId}/introduce")
+    @Operation(summary = "사용자 소개글 수정", description = "사용자의 소개글을 수정합니다.")
     public ResponseEntity<IntroduceResponse> submitNickname(@PathVariable Long memberId,
                                                             @RequestBody IntroduceRequest introduceRequest){
         IntroduceResponse updated = memberService.submitIntroduce(memberId, introduceRequest);
@@ -82,6 +90,7 @@ public class MemberController {
 
     // 사용자 프로필 입력 & 수정
     @PostMapping(value = "/members/{memberId}/profile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "사용자 프로필 사진 수정", description = "사용자의 프로필 사진을.")
     public ResponseEntity<String> fileUpload(@RequestParam("file") MultipartFile file, @PathVariable Long memberId) {
         try {
             String bucketName = "fzana"; // S3 버킷 이름 설정
