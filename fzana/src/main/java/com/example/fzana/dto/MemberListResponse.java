01f76355f3bd4b033346685129ad87bb36d5c4e1
kingjinyong <wlsdydwhswkf@gmail.com>
2024-04-26 16:45:47 +0900

    유저 검색 기능 구현 완료

---
+++ fzana/src/main/java/com/example/fzana/dto/MemberListResponse.java
@@ -0,0 +1,20 @@
+package com.example.fzana.dto;
+
+import com.example.fzana.domain.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MemberListResponse {
+    private Long userId;
+    private String name;
+
+    // DB에서 가져온 엔티티를 DTO로 반환하는 메서드
+    public static MemberListResponse createMember(Member member) {
+        return new MemberListResponse(
+                member.getId(),
+                member.getNickName());
+
+    }
+}
--- fzana/src/main/java/com/example/fzana/service/ScheduleService.java
+++ fzana/src/main/java/com/example/fzana/service/ScheduleService.java
@@ -34,7 +34,7 @@ public class ScheduleService {
         // 2. 엔티티 -> DTO 변환
         List<ScheduleResponse> dtos = new ArrayList<ScheduleResponse>();
         for (int i = 0; i < schedules.size(); i++){ // 1. 조회한 일정 엔티티 수 만큼 반복하기
-            Schedule s = schedules.get(i);          // 2. 조회한 댓글 엔티티 하나씩 가져오기
+            Schedule s = schedules.get(i);          // 2. 조회한 일정 엔티티 하나씩 가져오기
             ScheduleResponse dto = ScheduleResponse.createSchedule(s); // 3. 엔티티를 DTO로 변환
             dtos.add(dto);                          // 4. 변환한 DTO를 dtos 리스트에 삽입
         }
