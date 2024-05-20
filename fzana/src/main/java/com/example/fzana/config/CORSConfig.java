5951f3f41012040252c4372642d3e23bd5a18018
kingjinyong <wlsdydwhswkf@gmail.com>
2024-05-20 22:30:50 +0900

    CORS 서버 설정 수정

--- fzana/src/main/java/com/example/fzana/config/CORSConfig.java
+++ fzana/src/main/java/com/example/fzana/config/CORSConfig.java
@@ -10,7 +10,7 @@ public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
