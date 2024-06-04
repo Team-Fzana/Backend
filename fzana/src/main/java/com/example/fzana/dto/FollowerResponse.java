f5cbc0f3e63f6839656a270f94f3e8ad7750014a
kingjinyong <wlsdydwhswkf@gmail.com>
2024-06-04 19:01:22 +0900

    스웨거 example Value 수정, 팔로잉&팔로워 목록 가져오기 코드 리팩토링

---
+++ fzana/src/main/java/com/example/fzana/dto/FollowerResponse.java
@@ -0,0 +1,26 @@
+package com.example.fzana.dto;
+
+import com.example.fzana.domain.Follow;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FollowerResponse {
+    private Long followingId;
+    private String name;
+    private Boolean isActive;
+    private String memberPhoto;
+
+    public static FollowerResponse createMember(Follow f) {
+        return new FollowerResponse(
+                f.getFollower().getId(),
+                f.getFollower().getNickName(),
+                f.getFollower().getActive(),
+                f.getFollower().getMemberPhoto()
+        );
+    }
+}
