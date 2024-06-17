3faf0ddbd3eba08f4c517116ef503dd2e6a2391c
kingjinyong <wlsdydwhswkf@gmail.com>
2024-06-17 19:36:52 +0900

    사용자 정보 불러오기 부분 이메일 추가

--- fzana/src/main/java/com/example/fzana/dto/MemberInfoResponse.java
+++ fzana/src/main/java/com/example/fzana/dto/MemberInfoResponse.java
@@ -9,9 +9,10 @@ public class MemberInfoResponse {
     private String nickname;
     private String introduce;
     private String memberPhoto;
+    private String email;
     private Boolean active;
 
-    public static MemberInfoResponse createMemberinfoDto(String nickName, String introduce, String memberPhoto, Boolean active) {
-        return new MemberInfoResponse(nickName, introduce, memberPhoto, active);
+    public static MemberInfoResponse createMemberinfoDto(String nickName, String introduce, String memberPhoto, String email, Boolean active) {
+        return new MemberInfoResponse(nickName, introduce, memberPhoto, email, active);
     }
 }
