355f254b79311b5558985f47afe43fc8326e7e1d
kingjinyong <wlsdydwhswkf@gmail.com>
2024-12-26 18:05:45 +0900

    chore: ci-cd 테스트2

--- fzana/src/main/java/com/example/fzana/domain/goal/api/GoalController.java
+++ fzana/src/main/java/com/example/fzana/domain/goal/api/GoalController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/goalsss")
+@RequestMapping("/api/v1/goalssss")
 public class GoalController {
     private final GoalService goalService;
 
