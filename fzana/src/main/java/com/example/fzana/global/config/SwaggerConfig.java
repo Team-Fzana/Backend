799cf3f4f09f5dc687eb578816e9cc0637cf732a
kingjinyong <wlsdydwhswkf@gmail.com>
2024-12-30 10:51:36 +0900

    chore(SwaggerConfig): 스웨거 서버 설정

--- fzana/src/main/java/com/example/fzana/global/config/SwaggerConfig.java
+++ fzana/src/main/java/com/example/fzana/global/config/SwaggerConfig.java
@@ -1,13 +1,22 @@
 package com.example.fzana.global.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://siiso.com", description = "개발 서버"),
+                @Server(url = "http://localhost:8080", description = "로컬 서버")
+        })
+
 @Configuration
 public class SwaggerConfig {
+
 
     @Bean
     public OpenAPI customOpenAPI() {
