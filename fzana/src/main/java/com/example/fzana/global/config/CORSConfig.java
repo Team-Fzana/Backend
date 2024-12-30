4966d8d32e71e2fd904f6d201f27e05b78d72131
kingjinyong <wlsdydwhswkf@gmail.com>
2024-12-30 10:14:56 +0900

    chore(CORSConfig): CORS siiso.site 설정

--- fzana/src/main/java/com/example/fzana/global/config/CORSConfig.java
+++ fzana/src/main/java/com/example/fzana/global/config/CORSConfig.java
@@ -10,7 +10,7 @@ public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:8080")
+                .allowedOrigins("http://localhost:5173", "http://localhost:8080", "https://siiso.site")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
