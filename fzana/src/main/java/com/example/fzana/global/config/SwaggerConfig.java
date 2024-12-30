d8282fb6a0935a7ee0b4be86baca7e7bd772991d
kingjinyong <wlsdydwhswkf@gmail.com>
2024-12-30 11:01:21 +0900

    chore(SwaggerConfig): 스웨거 서버 설정

--- fzana/src/main/java/com/example/fzana/global/config/SwaggerConfig.java
+++ fzana/src/main/java/com/example/fzana/global/config/SwaggerConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         servers = {
-                @Server(url = "https://siiso.com", description = "개발 서버"),
+                @Server(url = "https://siiso.site", description = "개발 서버"),
                 @Server(url = "http://localhost:8080", description = "로컬 서버")
         })
 
